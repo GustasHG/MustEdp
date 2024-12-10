42a31b3b42ea936223f578475a7593e3665b7615
Tiago Andrade <90466869+ehotigas@users.noreply.github.com>
2024-12-10 09:39:11 -0300

    change api ip

--- src/api/ApiAdapter/ApiAdapter.ts
+++ src/api/ApiAdapter/ApiAdapter.ts
@@ -5,7 +5,7 @@ export interface ApiAdapterInterface {
 export class ApiAdapter implements ApiAdapterInterface {
     _url: string;
     public constructor() {
-        this._url = "http://172.20.71.20:4042";
+        this._url = "http://172.20.74.21:4042";
         // this._url = "http://localhost:4041";
     }
 
